b3db6cf9ca12fd964df733805f1f92ede5a1ffce
Seth Williamson <sethjwilliamson@gmail.com>
2022-07-27 15:26:40 -0600

    Create Deck Model

---
+++ packages/models/deck.ts
@@ -0,0 +1,34 @@
+import { DataTypes, Model } from "sequelize";
+import { getSequelizeInstance } from "./sequelize";
+
+const sequelize = getSequelizeInstance();
+
+export class Deck extends Model {
+  declare id: number;
+  declare deckCode: string;
+  declare createdAt: Date;
+  declare updatedAt: Date;
+}
+
+Deck.init(
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    deckCode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Deck",
+    underscored: true,
+  }
+);
+
+Deck.sync({ alter: true });
